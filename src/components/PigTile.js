ae7f0513cc7f7cd4c35c6d656a01dd15938fbc17
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-06-28 15:34:20 -0400

    sd

--- src/components/PigTile.js
+++ src/components/PigTile.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 class PigTile extends React.Component{
+
   constructor(){
     super()
     this.state = {
       clicked: false
     }
   }
+
   handleClick = () => {
     let toggledClick = !this.state.clicked
     this.setState({
@@ -14,25 +16,31 @@ class PigTile extends React.Component{
     })
   }
 
-  render(){
-    if (this.state.clicked)
-      { return (
-        <div className="pigTile" onClick={this.handleClick}>
+  getImgUrl = () => {
+    let sluggedName = (this.props.name.replace(/\s/g, '_')).toLowerCase()
+    console.log('wot')
+    return `/public/hog-imgs/${sluggedName}`
+  }
+
+  render() {
+    let style = {backgroundImage: "url('../../public/hog-imgs/cherub.jpg')"}
+    if (this.state.clicked) {
+      return (
+        <div className="pigTile" onClick={this.handleClick} style={style}>
           {this.props.name}
           <ul>
-          <li className="normalText">{this.props.specialty} specialist</li>
-          <li className="normalText">weighs {this.props.weight} refrigerators</li>
-          <li className="normalText">{this.props.medal} medal achieved</li>
+            <li className="normalText">{this.props.specialty} specialist</li>
+            <li className="normalText">weighs {this.props.weight} refrigerators</li>
+            <li className="normalText">{this.props.medal} medal achieved</li>
           </ul>
         </div>
       )
-      }
-    else {
-    return (
-      <div className="pigTile" onClick={this.handleClick}>
-        <div className="smallHeader">{this.props.name}</div>
-        <div>{this.props.specialty}</div>
-      </div>
+    } else {
+      return (
+        <div className="pigTile" onClick={this.handleClick} style={style}>
+          <div className="smallHeader">{this.props.name}</div>
+          <div>{this.props.specialty}</div>
+        </div>
       )
     }
   }
