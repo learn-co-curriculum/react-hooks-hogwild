e0ec36523892e98ed9a3eada3c0d2c58c25e78a2
Nicole D <dalcin.nicole@gmail.com>
2018-06-28 09:51:42 -0400

    refactor filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,55 +1,28 @@
 import React from "react";
 
-class Filter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sortOption: "all",
-      greased: false
-    };
-  }
-
-  handleSelectChange = e => {
-    let sort = e.target.value;
-    this.setState({ sortOption: sort });
-  };
-
-  handleCheck = e => {
-    this.setState({ greased: !this.state.greased });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onFilter(this.state);
-  };
-
-  render() {
-    return (
-      <div className="filterWrapper">
-        <form onSubmit={this.handleSubmit}>
-          <label>Sort By: </label>
-          <select
-            name="sort"
-            onChange={this.handleSelectChange}
-            value={this.state.sortOption}
-          >
-            <option value="all" />
-            <option value="name">Name</option>
-            <option value="weight">Weight</option>
-          </select>
-          <label>Greased Pigs Only?</label>
-          <input
-            checked={this.state.greased}
-            onChange={this.handleCheck}
-            type="checkbox"
-          />
-          <button className="btn btn-default" type="submit">
-            Filter
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+const Filter = props => {
+  return (
+    <div className="filterWrapper">
+      <form>
+        <label>Sort By: </label>
+        <select
+          name="sort"
+          onChange={props.handleSelectChange}
+          value={props.sortBy}
+        >
+          <option value="name">Name</option>
+          <option value="weight">Weight</option>
+        </select>
+        <label>Greased Pigs Only?</label>
+        <input
+          checked={props.greased}
+          onChange={props.handleToggleGreased}
+          type="checkbox"
+        />
+      </form>
+    </div>
+  );
+};
 
 export default Filter;
+
