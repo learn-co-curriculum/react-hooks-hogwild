443a2f961db9caf02b36f18f6a69fc66526f72ab
Nicole D <dalcin.nicole@gmail.com>
2018-06-28 09:52:13 -0400

    refactor hoglist

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -1,24 +1,12 @@
 import React from "react";
 import HogTile from "./HogTile";
 
-class HogList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      filter: null,
-      sort: null
-    };
-  }
-
-  render() {
-    let hogs = this.props.hogs;
-    return (
-      <div className="ui grid container">
-        {hogs.map(hog => <HogTile key={hog.name} hog={hog} />)}
-      </div>
-    );
-  }
-}
+const HogList = ({ hogs }) => {
+  return (
+    <div className="ui grid container">
+      {hogs.map(hog => <HogTile key={hog.name} hog={hog} />)}
+    </div>
+  );
+};
 
 export default HogList;
