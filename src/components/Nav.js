b5402b997d5709c92f0b3d88fb335d2a8cc0169d
Nicole D <dalcin.nicole@gmail.com>
2018-06-28 09:53:37 -0400

    added secret feature

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,12 +6,13 @@ const Nav = () => {
     <div className="navWrapper">
       <span className="headerText">Hogwarts</span>
       <div className="TwirlyPig">
-        <img src={piggy} className="App-logo" alt="piggy" />
+        <a href="https://www.homedepot.com/p/LG-Electronics-24-1-cu-ft-French-Door-Refrigerator-in-Stainless-Steel-LFX25974ST/202957132">
+          <img src={piggy} className="App-logo" alt="piggy" />
+        </a>
       </div>
       <span className="normalText">A React App for County Fair Hog Fans</span>
-      
     </div>
-  )
-}
+  );
+};
 
 export default Nav
