e4167f413830f248cd10139ec4b66ae6bb436aef
ihollander <ianholla@gmail.com>
2021-04-07 14:12:25 -0400

    Added image links

--- src/porkers_data.js
+++ src/porkers_data.js
@@ -1,88 +1,112 @@
 const hogs = [
   {
-    name: 'Babe',
-    specialty: 'Being incredibly cute',
+    name: "Babe",
+    specialty: "Being incredibly cute",
     greased: false,
     weight: 2.0,
-    'highest medal achieved': 'bronze'
+    "highest medal achieved": "bronze",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/babe.jpg",
   },
   {
-    name: 'Porkchop',
-    specialty: 'Making friends',
+    name: "Porkchop",
+    specialty: "Making friends",
     greased: true,
     weight: 1.6,
-    'highest medal achieved': 'silver'
+    "highest medal achieved": "silver",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/porkchop.jpg",
   },
   {
-    name: 'Cherub',
-    specialty: 'Flying',
+    name: "Cherub",
+    specialty: "Flying",
     greased: false,
     weight: 0.7,
-    'highest medal achieved': 'gold'
+    "highest medal achieved": "gold",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/cherub.jpg",
   },
   {
-    name: 'Piggy smalls',
-    specialty: 'Rapping',
+    name: "Piggy smalls",
+    specialty: "Rapping",
     greased: true,
     weight: 5.1,
-    'highest medal achieved': 'platinum'
+    "highest medal achieved": "platinum",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/piggy_smalls.jpg",
   },
   {
-    name: 'Trouble',
-    specialty: 'Racing',
+    name: "Trouble",
+    specialty: "Racing",
     greased: true,
     weight: 1.7,
-    'highest medal achieved': 'gold'
+    "highest medal achieved": "gold",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/trouble.jpg",
   },
   {
-    name: 'Piglet',
-    specialty: 'Bravery and friendship',
+    name: "Piglet",
+    specialty: "Bravery and friendship",
     greased: false,
     weight: 2.2,
-    'highest medal achieved': 'silver'
+    "highest medal achieved": "silver",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/piglet.jpg",
   },
   {
-    name: 'Peppa',
-    specialty: 'Being adventurous',
+    name: "Peppa",
+    specialty: "Being adventurous",
     greased: false,
     weight: 3.7,
-    'highest medal achieved': 'wood'
+    "highest medal achieved": "wood",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/peppa.jpg",
   },
   {
-    name: 'Truffle Shuffle',
-    specialty: 'Peeling oranges',
+    name: "Truffle Shuffle",
+    specialty: "Peeling oranges",
     greased: true,
     weight: 4.0,
-    'highest medal achieved': 'gold'
+    "highest medal achieved": "gold",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/truffle_shuffle.jpg",
   },
   {
-    name: 'Bailey',
-    specialty: 'Finding truffles',
+    name: "Bailey",
+    specialty: "Finding truffles",
     greased: false,
     weight: 2.3,
-    'highest medal achieved': 'bronze'
+    "highest medal achieved": "bronze",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/bailey.jpg",
   },
   {
-    name: 'Galaxy Note',
-    specialty: 'Cuddling',
+    name: "Galaxy Note",
+    specialty: "Cuddling",
     greased: true,
     weight: 1.9,
-    'highest medal achieved': 'diamond'
+    "highest medal achieved": "diamond",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/galaxy_note.jpg",
   },
   {
-    name: 'Leggo My Eggo',
-    specialty: 'Babysitting',
+    name: "Leggo My Eggo",
+    specialty: "Babysitting",
     greased: true,
     weight: 3.3,
-    'highest medal achieved': 'platinum'
+    "highest medal achieved": "platinum",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/leggo_my_eggo.jpg",
   },
   {
-    name: 'Augustus Gloop',
-    specialty: 'Making friends with ducks',
+    name: "Augustus Gloop",
+    specialty: "Making friends with ducks",
     greased: true,
     weight: 4.1,
-    'highest medal achieved': 'wood'
-  }
+    "highest medal achieved": "wood",
+    image:
+      "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwarts/master/public/images/augustus_gloop.jpg",
+  },
 ];
 
 export default hogs;
