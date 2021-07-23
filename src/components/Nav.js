028bce53554c48e0d25f70ca6012f94c8009f4e3
adam-larosa <adam.larosa@flatironschool.com>
2021-07-22 19:29:57 -0700

    Init

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,17 @@ import React from "react";
 import piggy from "../assets/porco.png";
 
 const Nav = () => {
-  return (
-    <div className="navWrapper">
-      <span className="headerText">Hogwarts</span>
-      <div className="TwirlyPig">
-        <img src={piggy} className="App-logo" alt="piggy" />
-      </div>
-      <span className="normalText">A React App for County Fair Hog Fans</span>
-    </div>
-  );
+	return (
+		<div className="navWrapper">
+			<span className="headerText">Hogwarts</span>
+			<div className="TwirlyPig">
+				<img src={piggy} className="App-logo" alt="piggy" />
+			</div>
+			<span className="normalText">
+				A React App for County Fair Hog Fans
+			</span>
+		</div>
+	);
 };
 
 export default Nav;
