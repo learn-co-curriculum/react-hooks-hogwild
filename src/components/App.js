7ef07bdab509e61d46c402f8a8b580b87fe3b41b
Emily Kanarek <emilyakanarek@gmail.com>
2025-06-13 11:01:38 -0400

    Sort by name or weight

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,38 @@ import hogs from "../porkers_data";
 
 function App() {
   const [hogList, setHogList] = useState(hogs);
+  const [sort, setSort] = useState("");
 
   const filterGreased = (checked) => {
     checked ? setHogList(hogs.filter((hog) => hog.greased)) : setHogList(hogs);
   };
 
+  const getSortedList = () => {
+    const listCopy = [...hogList];
+    if (sort === "name") {
+      return listCopy.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+    } else if (sort === "weight") {
+      return listCopy.sort((a, b) => a.weight - b.weight);
+    } else {
+      return listCopy;
+    }
+  };
+
+  const sortedList = getSortedList();
+
   return (
     <div className="App">
       <Nav />
-      <Filter filterGreased={filterGreased} />
-      <HogList hogs={hogList} />
+      <Filter filterGreased={filterGreased} setSort={setSort} />
+      <HogList hogs={sortedList} />
     </div>
   );
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 
-const Filter = ({ filterGreased }) => {
+const Filter = ({ filterGreased, setSort }) => {
   const [isChecked, setIsChecked] = useState(false);
 
-  const handleChange = () => {
+  const handleGreasedChange = () => {
     const checkedStatus = !isChecked;
     setIsChecked(checkedStatus);
     filterGreased(checkedStatus);
   };
 
+  const handleSortChange = (event) => {
+    const sortBy = event.target.value;
+    setSort(sortBy);
+  };
+
   return (
     <div className="filterWrapper">
-      <input type="checkbox" name="greasedOnly" onChange={handleChange} />
+      <input
+        type="checkbox"
+        name="greasedOnly"
+        onChange={handleGreasedChange}
+      />
       <label htmlFor="greasedOnly">Greased hogs only?</label>
+      <select name="sort" onChange={handleSortChange}>
+        <option value="">No Sort</option>
+        <option value="name">Sort By Name</option>
+        <option value="weight">Sort By Weight</option>
+      </select>
     </div>
   );
 };
