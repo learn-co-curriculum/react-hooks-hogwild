487f9ce2fda713e58ebcc764d0280fd584c19c79
Emily Kanarek <emilyakanarek@gmail.com>
2025-06-13 09:43:08 -0400

    Click hog tile to display more info

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -3,9 +3,16 @@ import HogTile from "./HogTile";
 
 const HogList = ({ hogs }) => {
   return (
-    <div>
+    <div className="ui grid container">
       {hogs.map((hog) => (
-        <HogTile key={hog.name} image={hog.image} name={hog.name} />
+        <HogTile
+          key={hog.name}
+          image={hog.image}
+          name={hog.name}
+          greased={hog.greased}
+          weight={hog.weight}
+          medal={hog["highest medal achieved"]}
+        />
       ))}
     </div>
   );
