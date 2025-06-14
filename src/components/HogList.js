c227c9eaa0f7c5a4d41906d3876a33990ee41c5b
Nihar Patel <niharpatel52@gmail.com>
2025-06-14 11:41:55 -0400

    display the other details about the hog

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function HogList({ hogs }) {
+  const [clickedHog, setClickedHog] = useState("");
+
+  const handleClick = (hog) => {
+    if (clickedHog === hog) {
+      setClickedHog("");
+    } else {
+      setClickedHog(hog);
+    }
+  };
+
+  const renderHogs = () => {
+    return hogs.map((hog) => (
+      <div onClick={() => handleClick(hog)}>
+        <h2>{hog.name}</h2>
+        <img src={hog.image} alt={hog.name} />
+        {clickedHog === hog && 
+        <div>
+          <p>Specialty: {hog.specialty}</p>
+          <p>Weight: {hog.weight}</p>
+          <p>Greased: {hog.greased ? "Yes" : "No"}</p>
+          <p>Highest Medal: Achieved {hog["highest medal achieved"]}</p>
+        </div>
+        }
+      </div>
+    ));
+  };
+
   return (
     <div>
-      {hogs.map((hog) => (
-        <div>
-          <h2>{hog.name}</h2>
-          <img src={hog.image} alt={hog.name} />
-        </div>
-      ))}
+      {renderHogs()}
     </div>
   );
 }
