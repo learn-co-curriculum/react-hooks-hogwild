c7e1d59cbd42483b41b2229fde84e8ce65727055
Nihar Patel <niharpatel52@gmail.com>
2025-06-15 16:24:02 -0400

    Complete Lab

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 function HogList({ hogs }) {
   const [clickedHog, setClickedHog] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [greased, setGreased] = useState(false);
 
   const handleClick = (hog) => {
     if (clickedHog === hog) {
@@ -11,8 +13,25 @@ function HogList({ hogs }) {
     }
   };
 
+  const handleSort = (type) => {
+    setSortBy(type);
+  };
+
+  const handleFilter = () => {
+    setGreased(!greased);
+  };
+
   const renderHogs = () => {
-    return hogs.map((hog) => (
+    let filteredHogs = greased ? hogs.filter(hog => hog.greased) : hogs;
+    let sortedHogs = [...filteredHogs];
+    
+    if (sortBy === "name") {
+      sortedHogs.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortBy === "weight") {
+      sortedHogs.sort((a, b) => a.weight - b.weight);
+    }
+
+    return sortedHogs.map((hog) => (
       <div onClick={() => handleClick(hog)}>
         <h2>{hog.name}</h2>
         <img src={hog.image} alt={hog.name} />
@@ -28,8 +47,17 @@ function HogList({ hogs }) {
     ));
   };
 
+  const sortButtons = (
+    <div>
+      <button onClick={() => handleSort("name")}>Sort by Name</button>
+      <button onClick={() => handleSort("weight")}>Sort by Weight</button>
+      <button onClick={handleFilter}>Show {greased ? "All" : "Greased"} Hogs</button>
+    </div>
+  );
+
   return (
     <div>
+      {sortButtons}
       {renderHogs()}
     </div>
   );
