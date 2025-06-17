074bc89493aa295b99be5a85f52478d8624ea409
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-06-17 09:40:52 -0400

    Sort by weight and height

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,20 @@ import hogs from "../porkers_data"
 
 function App() {
   const [showGreasedOnly, setShowGreasedOnly] = useState(false)
+  const [sortValue, setSortValue] = useState("none")
 
-  const toggleShowGreasedOnly = () => {
-    setShowGreasedOnly(!showGreasedOnly)
-  }
+  const toggleShowGreasedOnly = () => setShowGreasedOnly(!showGreasedOnly)
+  const handleSortChange = (event) => setSortValue(event.target.value)
 
   const filteredHogs = hogs.filter((hog) => {
     return showGreasedOnly ? hog.greased : true
+  })
+
+  const filteredAndSortedHogs = filteredHogs.sort((a, b) => {
+    const valueA = String(a[sortValue])
+    const valueB = String(b[sortValue])
+
+    return valueA.localeCompare(valueB)
   })
 
   // ** Alternate solution **
@@ -23,9 +30,12 @@ function App() {
 
   return (
     <div className="App">
-      <Nav onGreasedToggle={toggleShowGreasedOnly} />
+      <Nav
+        onGreasedToggle={toggleShowGreasedOnly}
+        handleSortChange={handleSortChange}
+      />
       <div className="ui grid container">
-        {filteredHogs.map((hog) => (
+        {filteredAndSortedHogs.map((hog) => (
           <Hog hog={hog} />
         ))}
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,24 @@
 import React from "react"
 import piggy from "../assets/porco.png"
 
-const Nav = ({ onGreasedToggle }) => {
+const Nav = ({ onGreasedToggle, handleSortChange }) => {
   return (
     <div className="navWrapper">
       <div class="ui checkbox">
         <input onChange={onGreasedToggle} type="checkbox" name="greased" />
         <label>Greased?</label>
       </div>
+
+      <br />
+      <br />
+
+      <label>Sort</label>
+      <select onChange={handleSortChange} class="ui fluid dropdown">
+        <option value="none">None</option>
+        <option value="name">Name</option>
+        <option value="weight">Weight</option>
+      </select>
+
       <span className="headerText">HogWild</span>
       <div className="TwirlyPig">
         <img src={piggy} className="App-logo" alt="piggy" />
