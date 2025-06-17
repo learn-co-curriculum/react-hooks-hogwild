252d13a504a2119356532ad4e52e30c106a8d5a5
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-06-17 09:17:50 -0400

    Filter by greased

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import Nav from "./Nav"
 import Hog from "./Hog"
 
 import hogs from "../porkers_data"
 
 function App() {
+  const [showGreasedOnly, setShowGreasedOnly] = useState(false)
+
+  const toggleShowGreasedOnly = () => {
+    setShowGreasedOnly(!showGreasedOnly)
+  }
+
+  const filteredHogs = hogs.filter((hog) => {
+    return showGreasedOnly ? hog.greased : true
+  })
+
+  // ** Alternate solution **
+  //
+  // const filteredHogs = showGreasedOnly
+  //   ? hogs.filter((hog) => hog.greased)
+  //   : hogs
+
   return (
     <div className="App">
-      <Nav />
+      <Nav onGreasedToggle={toggleShowGreasedOnly} />
       <div className="ui grid container">
-        {hogs.map((hog) => (
+        {filteredHogs.map((hog) => (
           <Hog hog={hog} />
         ))}
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
 import React from "react"
 import piggy from "../assets/porco.png"
 
-const Nav = () => {
+const Nav = ({ onGreasedToggle }) => {
   return (
     <div className="navWrapper">
+      <div class="ui checkbox">
+        <input onChange={onGreasedToggle} type="checkbox" name="greased" />
+        <label>Greased?</label>
+      </div>
       <span className="headerText">HogWild</span>
       <div className="TwirlyPig">
         <img src={piggy} className="App-logo" alt="piggy" />
