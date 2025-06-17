6f5e45c9da5fe74613b77c26e2f9325616e08a82
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-06-17 08:58:34 -0400

    Display hog details

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -1,14 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Hog({ hog }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div class="ui card">
+    <div onClick={handleClick} class="ui card">
       <div class="image">
         <img src={hog.image} />
       </div>
       <div class="content">
         <div class="header">{hog.name}</div>
       </div>
+      {isOpen && (
+        <>
+          <div>Specialty: {hog.specialty}</div>
+          <div>Weight: {hog.weight}</div>
+          <div>Greased: {hog.greased ? "Greased" : "Not greased"}</div>
+          <div>Highest Medal Achieved: {hog["highest medal achieved"]}</div>
+        </>
+      )}
     </div>
   )
 }
