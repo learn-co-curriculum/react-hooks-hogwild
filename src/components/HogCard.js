af97c9cd4e92de16d3faee5feabe54d45b31038f
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-21 14:49:46 -0400

    Create new component for hog details

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,14 +1,24 @@
 import React from "react"
+import HogDetails from "./HogDetails"
 
 function HogCard({name, specialty, greased, weight, image, medal}) {
   return (
-    <div className="card ui eight wide column">
+    <div className="card ui four wide column">
       <div className="image">
         <img src={image} />
       </div>
       <div className="content">
         <a className="header">{name}</a>
       </div>
+      <ol>
+        <HogDetails
+          name={name}
+          specialty={specialty}
+          greased={greased}
+          weight={weight}
+          medal={medal}
+        />
+      </ol>
     </div>
   )
 }
