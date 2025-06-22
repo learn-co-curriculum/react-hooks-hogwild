aa43abe42adf0bd50d28717ad07c9bbf3e9a3d30
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-21 22:21:59 -0400

    Add button that when clicked will hide the selected hog from the page

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -3,10 +3,18 @@ import HogDetails from "./HogDetails"
 
 function HogCard({name, specialty, greased, weight, image, medal}) {
   const [showDetails, setShowDetails] = useState(false)
+  const [hideHog, setHideHog] = useState(false)
 
   const handleClick = () => {
     setShowDetails(true)
   }
+
+  const handleHideHog = (event) => {
+    event.stopPropagation()
+    setHideHog(true)
+  }
+
+  if (hideHog) return null
 
   return (
     <div className="card ui four wide column" onClick={handleClick}>
@@ -15,6 +23,9 @@ function HogCard({name, specialty, greased, weight, image, medal}) {
       </div>
       <div className="content">
         <a className="header">{name}</a>
+      </div>
+      <div>
+        <button onClick={handleHideHog}>Hide Hog</button>
       </div>
       <div>
         {showDetails && (
