c2d796811d2038c2817761bd1941faedccc30737
Thomas Correia <thomas.correiap@gmail.com>
2026-06-12 19:02:00 -0400

    Render hog cards and toggle detailed hog information

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function HogCard({ hog }) {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <div>
+    <div onClick={() => setShowDetails(!showDetails)}>
       <h2>{hog.name}</h2>
-      <img src={hog.image} alt={hog.name} />
+
+      <img
+        src={hog.image}
+        alt={hog.name}
+      />
+
+      {showDetails && (
+        <div>
+          <p>Specialty: {hog.specialty}</p>
+          <p>Weight: {hog.weight}</p>
+          <p>Greased: {hog.greased ? "Yes" : "No"}</p>
+          <p>Medal: {hog.highest_medal_achieved}</p>
+        </div>
+      )}
     </div>
   );
 }
